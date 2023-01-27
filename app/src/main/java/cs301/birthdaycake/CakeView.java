886a11b048df6683230f182b84beabf09d7de7d9
Riley-Cameron <astoriacamfam@gmail.com>
2023-01-26 17:49:04 -0800

    two candles and frosting switch

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,8 +119,9 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        //Now 2 candles in the center
+        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2 - cakeWidth/6, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2 + cakeWidth/6, cakeTop);
 
     }//onDraw
 
